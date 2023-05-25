60e3f2c416a9fbd1ed8bb9e48dd9c0d2798a6c03
Ivan Kulikov <human.totem1@gmail.com>
2023-05-25 13:19:51 +0300

    Made a function

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,28 +1,4 @@
-﻿// Найти максимум из 9 чисел
-
-//int a1 = 15;
-//int b1 = 21;
-//int c1 = 39;
-//int a2 = 1202;
-//int b2 = 23;
-//int c2 = 33;
-//int a3 = 13;
-//int b3 = 23;
-//int c3 = 313;
-//
-//int max = a1;
-//if(b1 > max)  max = b1;
-//if(c1 > max)  max = c1;
-//
-//if(a2 > max)  max = a2;
-//if(b2 > max)  max = b2;
-//if(c2 > max)  max = c2;
-//
-//if(a3 > max)  max = a3;
-//if(b3 > max)  max = b3;
-//if(c3 > max)  max = c3;
-//
-//Console.WriteLine(max);
+﻿
 
 int Max(int arg1, int arg2, int arg3)
 {
@@ -31,15 +7,6 @@ int Max(int arg1, int arg2, int arg3)
     if(arg3> result) result = arg3;
     return result;
 }
-int a1 = 15;
-int b1 = 21;
-int c1 = 39;
-int a2 = 1202;
-int b2 = 23;
-int c2 = 33;
-int a3 = 13;
-int b3 = 23;
-int c3 = 313;
 
 //int max1 = Max(a1, b1, c1);
 //int max2 = Max(a2, b2, c2);
